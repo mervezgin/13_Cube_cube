133cef91f5f3139470b8f61937e05a186133a385
mervezgin <mervezgin@gmail.com>
2024-05-09 13:49:30 +0300

    added objecthit.cs to wall and when the player hit the wall, wall turned red

---
+++ Assets/Scripts/ObjectHit.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Bumped into a wall");
+            GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
+}
